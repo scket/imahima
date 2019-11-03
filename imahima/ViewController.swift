eddbfaafec01a953e25d0592d421a21495452fba
yuki goto <imahima.dev@gmail.com>
2019-11-03 19:37:07 +0900

    Initial Commit

---
+++ imahima/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  imahima
+//
+//  Created by yuki goto on 2019/11/03.
+//  Copyright © 2019 後藤祐貴. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
