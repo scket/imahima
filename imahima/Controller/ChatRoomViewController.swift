d6f683f3e125cdefb5aab371cd209a402300a76a
Shota Takeshima <>
2019-12-12 20:05:25 +0900

    チャット機能の追加(モック)

--- imahima/Controller/ChatRoomViewController.swift
+++ imahima/Controller/ChatRoomViewController.swift
@@ -8,11 +8,36 @@
 
 import UIKit
 
-class ChatRoomViewController: UIViewController {
-
+class ChatRoomViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    let chatRoomList = ["yugoto", "stakeshi"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        view.backgroundColor = UIColor.blue
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return chatRoomList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel!.text = chatRoomList[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // セルの選択を解除
+        tableView.deselectRow(at: indexPath, animated: true)
+        // 別の画面に遷移
+        self.transitionToChat()
+    }
+    
+    func transitionToChat() {
+        AppDelegate.shared.rootViewController.transitionToChat()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
     }
 }
